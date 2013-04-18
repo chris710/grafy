bce78758c919e9f4189f6dcdfd8436053be24583
chris710 <chris710@o2.pl>
2013-04-18 21:05:22 +0300

    UFFF NAPRAWIŁEM
    - dodana pętla programu
    - tworzenie grafu jako listy następników
    - dodana lista "TO DO NEXT"

--- main.cpp
+++ main.cpp
@@ -1,15 +1,55 @@
 #include<iostream>
 #include<stdlib>
+#include<list>              //w końcu działamy na listach c'nie?
 
 using namespace std;
 
-class herp
+
+
+//          DO ZROBIENIA:
+//  1)  coś do sprawdzania czy graf jest acykliczny
+//  2)  macierz sąsiedztwa
+//  3)  lista krawędzi zapisana jako tabela
+//  4)  sortowanie topologiczne (zgodnie z algorytmem przeszukiwania w głąb)
+//  5)  i magiczny zegarek mierzący ile to zajmuje
+//  6)  pętelka mierząca czas dla 10 wartości "liczba"
+
+
+
+struct vertex                                                           //struktura wierzchołka grafu dla listy następników (ta pierwsza pozycja)
 {
-        //asdfgadfbdfbv
+    int id;             //"numer" wierzchołka
+    vertex* next;       //wskaźnik na sąsiada
 };
 
-int main()
+struct vneigh                                                           //"vertex neighbour" czyli kolejne pozycje na liście następników
 {
-    cout << "Do dziela!!!" << endl;         //wywietla cos
+    int id;             //numer sąsiada
+    vneigh* next;       //wskaźnik na kolejnego sąsiada
+};
+
+int main()                                                              //main
+{
+    int liczba;         //liczba wierzchołków w tworzonym grafie
+    bool dalej=true;    //flaga dla pętli programu; jeżeli staje się false to program zostaje przerwany
+
+
+    while (dalej)       //pętla programu, dzięki temu można wykonać parę operacji bez wielokrotnego włączania programu
+    {
+        cout<<"Podaj liczbe vertexow grafu"<<endl;      //prosimy użytkownika o liczę danych
+        cin>>liczba;
+
+        vertex pierwszy = new vertex[liczba];       //tworzymy graf "pierwszy" zawierający "liczba" wierzchołków; jest to lista struktur
+
+        for(int i=0;i<liczba;i++)       //zapełniamy graf danymi
+        {
+            pierwszy[i].id=i;       //każdy wierzchołek dostaje własne id (lecą po kolei od 0)
+            pierwszy[i].next=NULL;  //chwilowo nie robimy żadnych krawędzi
+        }
+
+
+    }
+
+
     return 0;
 }
